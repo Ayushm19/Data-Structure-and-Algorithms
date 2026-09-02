49b61bc0a6dcf7a1177526514f7c76364c58593f
Ayush Mishra <knandan400@gmail.com>
2026-09-02 12:21:45 +0530

    Time: 287 ms (43.01%), Space: 30 MB (5.35%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -5,13 +5,15 @@ class Solution(object):
         deque_ = deque()
 
         for right in range(n):
+            #loop is removing indices that are outside the current sliding window
             while deque_ and deque_[0] <= right -k :
                 deque_.popleft()
-
+            #now we r checking if the num we r on is greater then deques num then del deque
             while deque_ and nums[deque_[-1]] < nums[right]:
                 deque_.pop()
             
             deque_.append(right)
+            #here we r checking if our sliding window has the target len or no 
             if right >= k-1 :
                 res[right -k + 1] = nums[deque_[0]]
         return res
